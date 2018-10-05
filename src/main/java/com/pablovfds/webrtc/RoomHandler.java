571508ba2085b6c8fe4fcf18f3bec64e5aa131ad
Pablo Victor <pablo.santos@ccc.ufcg.edu.br>
2018-10-04 22:29:56 -0300

    Implementando GroupCall utilizando Kurento

---
+++ src/main/java/com/pablovfds/webrtc/RoomHandler.java
@@ -0,0 +1,80 @@
+package com.pablovfds.webrtc;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import org.kurento.client.IceCandidate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+public class RoomHandler extends TextWebSocketHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(RoomHandler.class);
+
+    private static final Gson gson = new GsonBuilder().create();
+
+    @Autowired
+    private RoomManager roomManager;
+
+    @Autowired
+    private UserRegistry registry;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        final JsonObject jsonMessage = gson.fromJson(message.getPayload(), JsonObject.class);
+
+        String action = jsonMessage.get("action").getAsString();
+
+        final UserSession user = registry.getBySession(session);
+
+        switch (action) {
+            case "joinRoom":
+                joinRoom(jsonMessage, session);
+                break;
+            case "receiveVideoFrom":
+                final String senderName = jsonMessage.get("sender").getAsString();
+                final UserSession sender = registry.getByName(senderName);
+                final String sdpOffer = jsonMessage.get("sdpOffer").getAsString();
+                user.receiveVideoFrom(sender, sdpOffer);
+                break;
+            case "leaveRoom":
+                leaveRoom(user);
+                break;
+            case "onIceCandidate":
+                JsonObject candidate = jsonMessage.get("candidate").getAsJsonObject();
+
+                if (user != null) {
+                    IceCandidate cand = new IceCandidate(candidate.get("candidate").getAsString(),
+                            candidate.get("sdpMid").getAsString(), candidate.get("sdpMLineIndex").getAsInt());
+                    user.addCandidate(cand, jsonMessage.get("name").getAsString());
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void joinRoom(JsonObject jsonMessage, WebSocketSession session) throws IOException {
+        final String roomName = jsonMessage.get("room").getAsString();
+        final String name = jsonMessage.get("name").getAsString();
+        log.info("PARTICIPANT {}: trying to join room {}", name, roomName);
+
+        Room room = roomManager.getRoom(roomName);
+        final UserSession user = room.join(name, session);
+        registry.register(user);
+    }
+
+    private void leaveRoom(UserSession user) throws IOException {
+        final Room room = roomManager.getRoom(user.getRoomName());
+        room.leave(user);
+        if (room.getParticipants().isEmpty()) {
+            roomManager.removeRoom(room);
+        }
+    }
+}
---
+++ src/main/java/com/pablovfds/webrtc/WebRtcApplication.java
@@ -0,0 +1,43 @@
+package com.pablovfds.webrtc;
+
+import org.kurento.client.KurentoClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@SpringBootApplication
+@EnableWebSocket
+public class WebRtcApplication implements WebSocketConfigurer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRtcApplication.class, args);
+    }
+
+    @Bean
+    public UserRegistry registry() {
+        return new UserRegistry();
+    }
+
+    @Bean
+    public RoomManager roomManager() {
+        return new RoomManager();
+    }
+
+    @Bean
+    public RoomHandler groupRoomHandler() {
+        return new RoomHandler();
+    }
+
+    @Bean
+    public KurentoClient kurentoClient() {
+        return KurentoClient.create();
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(groupRoomHandler(), "/room").setAllowedOrigins("*");
+    }
+}
